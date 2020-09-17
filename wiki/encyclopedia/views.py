1aac8ff843092e8108f55e697a6478e746056cf0
Dylan K <dylan.kilpatrick92@gmail.com>
2020-09-17 00:38:15 -0400

    Index page list of entries are now URLs

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -14,8 +14,14 @@ def entry(request, entry):
     markdowner = Markdown()
     htmlEntry = markdowner.convert(util.get_entry(entry))
     #htmlEntry = htmlEntry[0:len(htmlEntry)]
-
     return render(request, "encyclopedia/entry.html", {
         "title": entry.capitalize(),
         "entry": htmlEntry
     })
+    """
+    try:
+        present the page the client is looking for
+    except TypeError: #  this is what was thrown when I tried using an incorrect URL
+        show them an error page tailored to their error
+    """
+    
