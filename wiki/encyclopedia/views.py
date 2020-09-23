a8b12b2fef7084b9a2c1262bdf5679685dd1182b
Dylan K <dylan.kilpatrick92@gmail.com>
2020-09-23 01:49:52 -0400

    Implemented search function. Added favicon.

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -13,11 +13,51 @@ def entry(request, entry):
 
     markdowner = Markdown()
     htmlEntry = markdowner.convert(util.get_entry(entry))
-    #htmlEntry = htmlEntry[0:len(htmlEntry)]
+
     return render(request, "encyclopedia/entry.html", {
         "title": entry.capitalize(),
         "entry": htmlEntry
     })
+
+def search(request):
+    
+    #store the client's search
+    search = request.GET['q']
+
+    # if the search string is a page, load that page
+    # but this seems to be causing only upper case to match
+    """
+    if search in util.list_entries():
+        return entry(request, search)
+    """
+    #trying this to avoid the issue with upper case
+    substrings = []
+    resultCount = 0
+
+    for pages in util.list_entries():
+        # if query matches name of an encyclopedia entry, display that entry
+        if search.lower() == pages.lower():
+            return entry(request, search)
+        elif search.lower() and pages.lower().find(search.lower()) >= 0:
+            substrings.append(pages.lower())
+
+    if len(substrings):
+        resultCount = len(substrings)
+            
+        # if the query does not match the name of an encyclopedia entry, the user should
+        # instead be taken to a search results page that displays a list of all encyclopedia
+        # entries that have the query as a substring. For example, if the search query were
+        # Py, then Python should appear in the search results.
+    
+    #if not pages match even the substring, show "0 results found"
+    #else:
+    title = f"Resuts for \"{search}\""
+    return render(request,"encyclopedia/search.html", {
+    "title": title,
+    "substrings": substrings,
+    "resultCount": resultCount
+    })
+
     """
     try:
         present the page the client is looking for
